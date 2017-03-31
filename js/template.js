625182f51af3bbad4acae39b431a7d0839973385
zfxmnb <664127999@qq.com>
2017-03-31 16:47:16 +0800

    Update template.js

--- js/template.js
+++ js/template.js
@@ -1,6 +1,6 @@
- function template(data, t, e) {
+function template(data, t, e) {
         var tpl = document.getElementById(t),ele = document.getElementById(e),
-            a = tpl.value.split(/{{|}}/),s = "var outStr='';",i = 0;
+            a = tpl.value.split(/<%|%>/),s = "var outStr='';",i = 0;
         while (i < a.length) {
             if (i % 2 == 0) {
                 s += "outStr+='" + a[i].replace(/[\r\n]/g, "") + "';";
