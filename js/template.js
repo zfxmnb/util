c58b16fa5087cba3c2e8dd4a72d053e08bdc6d0d
zfxmnb <664127999@qq.com>
2017-03-31 12:50:50 +0800

    Update template.js

--- js/template.js
+++ js/template.js
@@ -1,22 +1,20 @@
-function template(data,t,e) {
-    var tpl = document.getElementById(t),
-        ele = document.getElementById(e);
-    var arr=tpl.value.split(/<%|%>/);
-    var str="var string='';";
-    for(var i in arr){
-        if(i%2==0){
-            if(arr!="")
-                str+="string+='"+arr[i].replace(/\s*/g, "")+"';";
-        }else{
-            if(arr[i].indexOf("=")==0){
-                 str+="string+="+arr[i].substr(1)+";";
-            }else{
-                str+=arr[i];
-            }
+function template(data, t, e) {
+    var tpl = document.getElementById(t),ele = document.getElementById(e),
+        a = tpl.value.split(/<%|%>/),s = "var outStr='';",i = 0;
+    while (i < a.length) {
+        if (i % 2 == 0) {
+            if (a != "")
+                s += "outStr+='" + a[i].replace(/\s*/g, "") + "';";
+        } else {
+            if (a[i].indexOf("=") == 0)
+                s += "outStr+=" + a[i].substr(1) + ";";
+            else
+                s += a[i];
         }
+        i++;
     }
-    eval(str);
-    if(ele)
-        ele.innerHTML = string;
-    return string;
+    eval(s);
+    if (ele)
+        ele.innerHTML = outStr;
+    return outStr;
 }
