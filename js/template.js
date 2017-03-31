0228d33de71cf3d7e5af44b208ce4d4c2324fbb5
zfxmnb <664127999@qq.com>
2017-03-31 16:44:55 +0800

    Update template.js
    修复bug

--- js/template.js
+++ js/template.js
@@ -1,20 +1,19 @@
-function template(data, t, e) {
-    var tpl = document.getElementById(t),ele = document.getElementById(e),
-        a = tpl.value.split(/<%|%>/),s = "var outStr='';",i = 0;
-    while (i < a.length) {
-        if (i % 2 == 0) {
-            if (a != "")
-                s += "outStr+='" + a[i].replace(/^\s*|\s*$/, "") + "';";
-        } else {
-            if (a[i].indexOf("=") == 0)
-                s += "outStr+=" + a[i].substr(1) + ";";
-            else
-                s += a[i];
+ function template(data, t, e) {
+        var tpl = document.getElementById(t),ele = document.getElementById(e),
+            a = tpl.value.split(/{{|}}/),s = "var outStr='';",i = 0;
+        while (i < a.length) {
+            if (i % 2 == 0) {
+                s += "outStr+='" + a[i].replace(/[\r\n]/g, "") + "';";
+            } else {
+                if (a[i].indexOf("=") == 0)
+                    s += "outStr+=" + a[i].substr(1) + ";";
+                else
+                    s += a[i];
+            }
+            i++;
         }
-        i++;
+        eval(s);
+        if (ele)
+            ele.innerHTML = outStr;
+        return outStr;
     }
-    eval(s);
-    if (ele)
-        ele.innerHTML = outStr;
-    return outStr;
-}
