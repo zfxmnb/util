e16319a557f13d7973b119205153b0ea932912f9
zfxmnb <664127999@qq.com>
2017-03-31 17:20:04 +0800

    Update template.js
    优化性能

--- js/template.js
+++ js/template.js
@@ -3,7 +3,8 @@ function template(data, t, e) {
             a = tpl.value.split(/<%|%>/),s = "var outStr='';",i = 0;
         while (i < a.length) {
             if (i % 2 == 0) {
-                s += "outStr+='" + a[i].replace(/[\r\n]/g, "") + "';";
+                if(/\S/.test(a[i]))
+                    s += "outStr+='" + a[i].replace(/[\r\n]/g, "") + "';";
             } else {
                 if (a[i].indexOf("=") == 0)
                     s += "outStr+=" + a[i].substr(1) + ";";
