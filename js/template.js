091de59971c188e7c475a513223a7404a036d65c
zfxmnb <664127999@qq.com>
2017-04-01 11:05:19 +0800

    Update template.js
    add new function

--- js/template.js
+++ js/template.js
@@ -1,8 +1,8 @@
-function template(data, t, e) {
+function templateOld(data, t, e) {
         var tpl = document.getElementById(t),ele = document.getElementById(e),
             a = tpl.value.split(/<%|%>/),s = "var outStr='';",i = 0;
         while (i < a.length) {
-            if (i % 2 == 0) {
+            if (i % 2 === 0) {
                 if(/\S/.test(a[i]))
                     s += "outStr+='" + a[i].replace(/[\r\n]/g, "") + "';";
             } else {
@@ -13,7 +13,26 @@ function template(data, t, e) {
             }
             i++;
         }
-        eval(s);
+        try{
+            eval(s);
+        }catch(e){
+            console.error(e);
+            outStr="{template error}";
+        }
+        if (ele)
+            ele.innerHTML = outStr;
+        return outStr;
+    }
+function template(data, t, e) {
+        var tpl = document.getElementById(t),
+            ele = document.getElementById(e),
+            s ="var outStr='"+tpl.value.replace(/[\r\n]/g, "").replace(/<%=([^<%=]*)%>/g, "';outStr+=$1;outStr+='").replace(/<%([^<%]*)%>/g,"';$1;outStr+='")+"';";
+        try{
+            eval(s);
+        }catch(e){
+            console.error(e);
+            outStr="{template error}";
+        }
         if (ele)
             ele.innerHTML = outStr;
         return outStr;
